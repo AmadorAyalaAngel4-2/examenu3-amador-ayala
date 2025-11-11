f96a88596e15dbfd89ac1dd76f6a954521404f12
AmadorAyalaAngel4-2 <a24311010@uthermosillo.edu.mx>
2025-11-10 20:34:05 -0700

    otro arreglo

--- sistema_tienda/settings.py
+++ sistema_tienda/settings.py
@@ -85,10 +85,10 @@ DATABASES = {
         'PASSWORD': 'AAAA060113_carlo',
         'HOST': 'examenu3AmadorAyalaAngel.mysql.pythonanywhere-services.com',
         'PORT': '3306',
-        'OPTIONS': {
-            'charset': 'utf8mb4',
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'; SET innodb_strict_mode=1;",
-        },
+        #'OPTIONS': {
+        #    'charset': 'utf8mb4',
+        #    'init_command': "SET sql_mode='STRICT_TRANS_TABLES'; SET innodb_strict_mode=1;",
+        #},
         'TIME_ZONE': None,
         'TEST': {
             'CHARSET': 'utf8mb4',
